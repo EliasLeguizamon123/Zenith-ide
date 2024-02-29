d22e9f3ab7c954df5e18c01b3e35464b74723eb8
Elias Leguizamon <elias.leguizamon1997@gmail.com>
2024-02-29 11:54:14 -0300

    feat(sections): add sections, set colorScheme as catpuccin frape

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -5,7 +5,28 @@ const plugins = require('tailwindcss/plugin')
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				'rosewater': '#f4dbd6',
+				'flamingo': '#f0c6c6',
+				'maroon': '#ee99a0',
+				'peach': '#f5a97f',
+				'sunflower': '#eed49f',
+				'woods': '#a6da95',
+				'sky': '#91d7e3',
+				'text': '#cad3f5',
+				'subtext': '#a5adcb',
+				'overlay': '#6e738d',
+				'surface': '#363a4f',
+				'base': '#24273a',
+				'mantle': '#1e2030',
+				'crust': '#181926',
+			}
+		},
+		borderWidth: {
+			'1': '1px',
+			'2': '2px'
+		}
 	},
 	plugins: [],
 }

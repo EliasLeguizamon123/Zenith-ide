442ca54a8fe9d0b1b2e50f4088b4835518a4ff5e
Elias Leguizamon <elias.leguizamon1997@gmail.com>
2024-03-11 13:27:58 -0300

    chore(production): now app works in production statement

--- dist-electron/main.js
+++ dist-electron/main.js
@@ -1,23 +1,54 @@
-import * as t from "url";
-import { app as i, BrowserWindow as o, ipcMain as n } from "electron";
-i.whenReady().then(() => {
-  const e = new o({
+import * as p from "url";
+import { fileURLToPath as c } from "url";
+import { app as t, BrowserWindow as d, ipcMain as o } from "electron";
+import { exec as f } from "child_process";
+import * as i from "path";
+import { join as r } from "path";
+import * as u from "os";
+import * as h from "fs";
+const R = c(import.meta.url), s = i.dirname(R), _ = {
+  // /dist
+  dist: r(s, "../.."),
+  // /dist or /public
+  public: r(s, t.isPackaged ? "../.." : "../../../public")
+}, w = r(_.dist, "dist/index.html");
+t.whenReady().then(() => {
+  const e = new d({
     title: "Main window",
     frame: !1,
-    transparent: !0,
+    transparent: !1,
     minHeight: 600,
     minWidth: 800,
     webPreferences: {
-      preload: t.fileURLToPath(new URL("preload.mjs", import.meta.url)),
+      preload: p.fileURLToPath(new URL("preload.mjs", import.meta.url)),
       nodeIntegration: !0,
       contextIsolation: !0
     }
   });
-  process.env.VITE_DEV_SERVER_URL ? e.loadURL(process.env.VITE_DEV_SERVER_URL) : e.loadFile("dist/index.html"), n.on("closeApp", () => {
-    i.quit();
-  }), n.on("minimizeApp", () => {
+  process.env.VITE_DEV_SERVER_URL ? e.loadURL(process.env.VITE_DEV_SERVER_URL) : e.loadFile(w), o.on("closeApp", () => {
+    t.quit();
+  }), o.on("minimizeApp", () => {
     e.minimize();
-  }), n.on("setFullScreen", () => {
+  }), o.on("setFullScreen", () => {
     e.setFullScreen(!e.isFullScreen());
-  });
+  }), E();
 });
+const E = async () => {
+  f("ls -la", (e, l, F) => {
+    if (e) {
+      console.error(e);
+      return;
+    }
+    const a = i.join(u.homedir(), "Desktop"), m = i.join(a, "carpeta.txt");
+    h.writeFile(m, l, (n) => {
+      if (n) {
+        console.error(n);
+        return;
+      }
+      console.log("File created");
+    });
+  });
+};
+export {
+  _ as ROOT_PATH
+};
--- src/electron/main.ts
+++ src/electron/main.ts
@@ -1,12 +1,29 @@
-// src/electron/main.ts
 import * as url from "url";
 import { app, BrowserWindow, ipcMain } from "electron";
+import { exec } from 'child_process';
+import { join } from 'path';
+import * as os from 'os';
+import * as path from 'path';
+import * as fs  from 'fs';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export const ROOT_PATH = {
+  // /dist
+  dist: join(__dirname, '../..'),
+  // /dist or /public
+  public: join(__dirname, app.isPackaged ? '../..' : '../../../public'),
+};
+
+const indexHtml = join(ROOT_PATH.dist, 'dist/index.html');
 
 app.whenReady().then(() => {
    const win = new BrowserWindow({
     title: "Main window",
     frame: false,
-    transparent: true,
+    transparent: false,
     minHeight: 600,
     minWidth: 800,
     webPreferences: {
@@ -22,7 +39,8 @@ app.whenReady().then(() => {
     // win.webContents.openDevTools();
   } else {
     // Load your file
-    win.loadFile("dist/index.html");
+    win.loadFile(indexHtml);
+
   }
 
 
@@ -37,5 +55,30 @@ app.whenReady().then(() => {
   ipcMain.on('setFullScreen', () => {
     win.setFullScreen(!win.isFullScreen());
   })
+
+  checkFolders();
   
 });
+
+
+const checkFolders = async () => {
+  
+  exec('ls -la', (err, stdout, stderr) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+
+    const desktopPath = path.join(os.homedir(), 'Desktop');
+    const filepath = path.join(desktopPath, 'carpeta.txt');
+
+    fs.writeFile(filepath, stdout, (err) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+      console.log('File created');
+    })
+    
+  })
+}
